c486f193ae879923ee11527b5d64fe973aaaf4fe
junqirao <jackrabbit872568318@aliyun.com>
2022-05-10 15:42:45 +0800

    配置推送

---
+++ config/pushed.go
@@ -0,0 +1,26 @@
+package config
+
+import "tunn-hub/config/protocol"
+
+//
+// PushedConfig
+// @Description:
+//
+type PushedConfig struct {
+	Global      PushedGlobal `json:"global"`
+	Routes      []Route      `json:"route"`
+	Device      Device       `json:"device"`
+	DataProcess DataProcess  `json:"data_process"`
+}
+
+//
+// PushedGlobal
+// @Description:
+//
+type PushedGlobal struct {
+	Address         string        `json:"address"`
+	Port            int           `json:"port"`
+	Protocol        protocol.Name `json:"protocol"`
+	Mtu             int           `json:"mtu"`
+	MultiConnection int           `json:"multi_connection"`
+}
