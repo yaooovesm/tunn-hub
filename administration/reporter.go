8f1b0eb2d6de719badfc47d9e686164e0ca8e6e9
junqirao <jackrabbit872568318@aliyun.com>
2022-05-20 10:12:07 +0800

    修复ReporterClient未正常关闭问题

--- administration/reporter.go
+++ administration/reporter.go
@@ -2,7 +2,6 @@ package administration
 
 import (
 	"encoding/json"
-	"fmt"
 	log "github.com/cihub/seelog"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -193,7 +192,6 @@ func (r *Reporter) HandleReportDispatch(ws *websocket.Conn) {
 			}.Byte())
 			break
 		}
-		fmt.Println("push --> ", string(marshal))
 		_, err = conn.Write(marshal)
 		if err != nil {
 			break
