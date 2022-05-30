401fc398af114ddf847011ad6a6953dd4cd8e2b4
junqirao <jackrabbit872568318@aliyun.com>
2022-05-30 09:23:35 +0800

    移除无用代码

--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -2,29 +2,7 @@ package tunnel
 
 import (
 	"net"
-	"tunn-hub/config"
 )
-
-//
-// ClientConnHandler
-// @Description:
-//
-type ClientConnHandler interface {
-	//
-	// AfterInitialize
-	// @Description:
-	// @param client
-	//
-	AfterInitialize(client *Client)
-	//
-	// CreateAndSetup
-	// @Description:
-	// @param Address
-	// @return conn
-	// @return err
-	//
-	CreateAndSetup(address string, config config.Config) (conn net.Conn, err error)
-}
 
 //
 // ServerConnHandler
