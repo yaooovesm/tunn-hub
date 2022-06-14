0143c29de384a1f5bb34c3931efea03969544402
junqirao <jackrabbit872568318@aliyun.com>
2022-06-14 14:10:52 +0800

    添加流量记录定时任务

--- administration/schedules.go
+++ administration/schedules.go
@@ -1,5 +1,15 @@
 package administration
 
+import (
+	"encoding/json"
+	log "github.com/cihub/seelog"
+	"io/ioutil"
+	"os"
+	"time"
+	"tunn-hub/administration/model"
+	"tunn-hub/config"
+)
+
 //
 // setupSchedules
 // @Description:
@@ -29,24 +39,94 @@ func setupSchedules(service *scheduleService) {
 	//	_ = log.Warn("flow_recorder register failed : ", err)
 	//	return
 	//}
+	//daily recorder
+	if config.Current.Schedule.DailyFlowRecord {
+		err := service.Register("@daily", "daily_flow_recorder", func() {
+			storagePath := "./record/daily/"
+			s, err := os.Stat(storagePath)
+			if os.IsNotExist(err) || !s.IsDir() {
+				_ = os.MkdirAll(storagePath, 0644)
+			}
+			err = dumpFlowData(storagePath+"daily_"+time.Now().Format("2006_01_02")+".record", false)
+			if err != nil {
+				_ = log.Warn("failed to dump current flow counter : ", err)
+			}
+		}, true)
+		if err != nil {
+			_ = log.Warn("schedule register failed : ", err)
+		}
+	}
+	//weekly recorder
+	if config.Current.Schedule.WeeklyFlowRecord {
+		err := service.Register("@weekly", "weekly_flow_recorder", func() {
+			storagePath := "./record/weekly/"
+			s, err := os.Stat(storagePath)
+			if os.IsNotExist(err) || !s.IsDir() {
+				_ = os.MkdirAll(storagePath, 0644)
+			}
+			err = dumpFlowData(storagePath+"weekly_"+time.Now().Format("2006_01_02")+".record", false)
+			if err != nil {
+				_ = log.Warn("failed to dump current flow counter : ", err)
+			}
+		}, true)
+		if err != nil {
+			_ = log.Warn("schedule register failed : ", err)
+		}
+	}
+	//monthly recorder
+	if config.Current.Schedule.MonthlyFlowRecord {
+		err := service.Register("@monthly", "monthly_flow_recorder", func() {
+			storagePath := "./record/monthly/"
+			s, err := os.Stat(storagePath)
+			if os.IsNotExist(err) || !s.IsDir() {
+				_ = os.MkdirAll(storagePath, 0644)
+			}
+			err = dumpFlowData(storagePath+"monthly_"+time.Now().Format("2006_01_02")+".record", config.Current.Schedule.MonthlyFlowReset)
+			if err != nil {
+				_ = log.Warn("failed to dump current flow counter : ", err)
+			}
+		}, true)
+		if err != nil {
+			_ = log.Warn("schedule register failed : ", err)
+		}
+	}
+
 }
 
-////
-//// recordAndResetFlow
-//// @Description:
-////
-//func recordAndResetFlow() {
-//	infos, err := UserServiceInstance().ListUserInfo()
-//	if err != nil {
-//		return
-//	}
-//	for i := range infos {
-//		info := model.UserInfo{
-//			Id: infos[i].Id,
-//		}
-//		err := UserServiceInstance().ResetFlowCounter(&info)
-//		if err != nil {
-//			return
-//		}
-//	}
-//}
+//
+// dumpFlowData
+// @Description:
+// @param file
+//
+func dumpFlowData(file string, reset bool) error {
+	infos, err := UserServiceInstance().ListUserInfo()
+	if err != nil {
+		return err
+	}
+	data := map[string]interface{}{}
+	for i := range infos {
+		info := model.UserInfo{
+			Id: infos[i].Id,
+		}
+		data[info.Id] = map[string]interface{}{
+			"account": infos[i].Account,
+			"rx":      infos[i].FlowCount,
+			"tx":      infos[i].TXCount,
+		}
+		if reset {
+			err := UserServiceInstance().ResetFlowCounter(&info)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(file, bytes, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
