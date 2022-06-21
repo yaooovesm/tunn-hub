5ae35000d7a8d558571c2d6c65e451f02edb1054
junqirao <jackrabbit872568318@aliyun.com>
2022-06-21 17:44:45 +0800

    网络检查定时任务

--- administration/schedules.go
+++ administration/schedules.go
@@ -16,30 +16,76 @@ import (
 // @param service
 //
 func setupSchedules(service *scheduleService) {
-	//err := service.Register("@every 30s", "flow_recorder", func() {
-	//	if ServerServiceInstance() == nil {
+	////每小时保存一次流量记录,总共保留24小时内记录
+	//err := service.Register("@hourly", "hourly_traffic_recorder", func() {
+	//	//确定要保存的文件名
+	//	//找到最新的序号，最新序号+1即为保存的序号
+	//	path := "./record/hourly/"
+	//	if config.Current.Schedule.HourlyNetworkTrafficDump != "" {
+	//		path = config.Current.Schedule.HourlyNetworkTrafficDump
+	//	}
+	//	s, err := os.Stat(path)
+	//	if os.IsNotExist(err) || !s.IsDir() {
+	//		_ = os.MkdirAll(path, 0644)
+	//	}
+	//	var latest = int64(0)
+	//	var latestNum = 0
+	//	var has = false
+	//	err = filepath.Walk(path,
+	//		func(p string, f os.FileInfo, err error) error {
+	//			if f != nil && !f.IsDir() {
+	//				mo := f.ModTime().UnixMilli()
+	//				num, err := strconv.Atoi(strings.ReplaceAll(filepath.Base(p), "traffic", ""))
+	//				if err != nil {
+	//					return err
+	//				}
+	//				if mo > latest {
+	//					latest = mo
+	//					latestNum = num
+	//					has = true
+	//				}
+	//			}
+	//			return nil
+	//		})
+	//	if err != nil {
+	//		_ = log.Warn("hourly record failed : ", err)
 	//		return
 	//	}
-	//	rx := uint64(0)
-	//	tx := uint64(0)
-	//	if ServerServiceInstance().rxFlowCounter != nil {
-	//		rx = ServerServiceInstance().rxFlowCounter.FlowSpeed
+	//	//确认序号写入
+	//	if has {
+	//		if latestNum == 23 {
+	//			latestNum = 0
+	//		} else {
+	//			latestNum += 1
+	//		}
 	//	}
-	//	if ServerServiceInstance().txFlowCounter != nil {
-	//		tx = ServerServiceInstance().txFlowCounter.FlowSpeed
+	//	err = ServerServiceInstance().recorderService.TrafficRecorder.DumpAndReset(path + "traffic" + strconv.Itoa(latestNum))
+	//	if err != nil {
+	//		_ = log.Warn("hourly record failed : ", err)
+	//		return
 	//	}
-	//	fmt.Println()
-	//	fmt.Println("-------------------------")
-	//	fmt.Println("rx <-- ", rx)
-	//	fmt.Println("tx <-- ", tx)
-	//	fmt.Println("-------------------------")
-	//	fmt.Println()
+	//}, true)
+	////每分钟记录一次流量记录
+	//err = service.Register("*/1 * * * *", "traffic_recorder", func() {
+	//	ServerServiceInstance().recorderService.TrafficRecorder.Push(
+	//		monitor.CreateHubTrafficStampWithDuration(
+	//			ServerServiceInstance().rxFlowCounter,
+	//			ServerServiceInstance().txFlowCounter,
+	//			60000, //记录时长 60000ms
+	//		),
+	//	)
 	//}, true)
 	//if err != nil {
-	//	_ = log.Warn("flow_recorder register failed : ", err)
-	//	return
+	//	_ = log.Warn("schedule register failed : ", err)
 	//}
-	//daily recorder
+	//网络导入检查
+	err := service.Register("*/1 * * * *", "user_imports_check_1m", func() {
+		checkUserImports()
+	}, true)
+	if err != nil {
+		_ = log.Warn("schedule register failed : ", err)
+	}
+	//日度流量记录
 	if config.Current.Schedule.DailyFlowRecord {
 		err := service.Register("@daily", "daily_flow_recorder", func() {
 			storagePath := "./record/daily/"
@@ -56,7 +102,7 @@ func setupSchedules(service *scheduleService) {
 			_ = log.Warn("schedule register failed : ", err)
 		}
 	}
-	//weekly recorder
+	//周度流量记录
 	if config.Current.Schedule.WeeklyFlowRecord {
 		err := service.Register("@weekly", "weekly_flow_recorder", func() {
 			storagePath := "./record/weekly/"
@@ -73,7 +119,7 @@ func setupSchedules(service *scheduleService) {
 			_ = log.Warn("schedule register failed : ", err)
 		}
 	}
-	//monthly recorder
+	//月度流量记录/重置
 	if config.Current.Schedule.MonthlyFlowRecord {
 		err := service.Register("@monthly", "monthly_flow_recorder", func() {
 			storagePath := "./record/monthly/"
@@ -129,4 +175,56 @@ func dumpFlowData(file string, reset bool) error {
 		return err
 	}
 	return nil
+}
+
+//
+// checkUserImports
+// @Description:
+//
+func checkUserImports() {
+	if UserServiceInstance().configService.UpdatedRecently {
+		list, err := UserServiceInstance().infoService.ListFull()
+		if err != nil {
+			_ = log.Warn("error during user imports check schedule : ", err)
+			return
+		}
+		for i := range list {
+			var changed = false
+			exports, err := UserServiceInstance().AvailableExports(list[i].Account)
+			if err != nil {
+				continue
+			}
+			for j := 0; j < len(list[i].Config.Routes); j++ {
+				if !importValidationCheck(exports, list[i].Config.Routes[j]) {
+					changed = true
+					list[i].Config.Routes = append(list[i].Config.Routes[:j], list[i].Config.Routes[j+1:]...)
+				}
+			}
+			if changed {
+				_, _ = UserServiceInstance().configService.UpdateById(model.ClientConfig{
+					Id:     list[i].ConfigId,
+					Routes: list[i].Config.Routes,
+					Device: list[i].Config.Device,
+					Limit:  list[i].Config.Limit,
+				}, false)
+			}
+		}
+		UserServiceInstance().configService.UpdatedRecently = false
+	}
+}
+
+//
+// importValidationCheck
+// @Description:
+// @param ava
+// @param route
+// @return bool
+//
+func importValidationCheck(ava []model.ImportableRoute, route config.Route) bool {
+	for i := range ava {
+		if ava[i].Network == route.Network {
+			return true
+		}
+	}
+	return false
 }
